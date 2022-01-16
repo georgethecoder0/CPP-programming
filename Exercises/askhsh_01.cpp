b7add2e20ab5535d2056516a40a652cf1a84921e
George Diakos <97686078+georgethecoder0@users.noreply.github.com>
2022-01-16 16:36:34 +0200

    Update askhsh_01.cpp

--- Exercises/askhsh_01.cpp
+++ Exercises/askhsh_01.cpp
@@ -1,8 +1,21 @@
 // Γράψε ενα πρόγραμμα στην C++ που να εκτυπώνει τα γράμματα απο a-z. ( Αντικατέστησε τον παρακάτω κώδικα με την λύση σου ) .
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main() {
-	printf("Hello World! \n");
-	return 0;
+int main(){
+    bool x = true;
+
+    
+    cout<<x<<endl;        
+
+  
+    x = 4;
+    cout<<x<<endl;        
+ 
+    x = !(0>1);    
+    if (x)
+        cout<<x<<endl;    
+
+    return 0;
 }
